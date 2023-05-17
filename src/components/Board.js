fdfcfa9a63d829011fd7023b9fb491ef58d866ab
daseul <7581058@gmail.com>
2023-05-17 16:22:29 +0900

    배포

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 import './Board.css'
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setxIsNext] = useState(true)
-  const status = `Next Player ${xIsNext ? 'X' : 'O'} `
-  const handleClick = (i) => {
-    const newSquares = squares.slice()
-    newSquares[i] = xIsNext ? 'X' : 'O'
-    setSquares(newSquares)
-    setxIsNext(prev => !prev)
-  }
-
+const Board = ({ squares, onClick }) => {
   const renderSquare = (i) => {
     return <Square value={squares[i]} 
-              onClick={() => handleClick(i)} 
+              onClick={() => onClick(i)} 
            />
   }
 
   return (
-    <div>
-        <div className='status'>{status}</div>
+    <div className='board-wrapper'>
         <div className='board-row'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className='board-row'>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className='board-row'>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
     </div>
   )
